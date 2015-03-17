6c52fa5859485c76d457f01ad95e5f4125241070
eigo <eigo@eigos-mbp.lan>
2015-03-17 16:30:22 +0200

    .

--- WordFall/SKNodes/MWStreamNode.h
+++ WordFall/SKNodes/MWStreamNode.h
@@ -9,19 +9,23 @@
 #import <SpriteKit/SpriteKit.h>
 
 @class MWStreamNode;
+@class MWObjects;
 
 typedef void (^MWStreamMaxDistanceReached)(MWStreamNode *stream);
 typedef void (^MWStreamLetterTouched)(MWStreamNode *stream);
 
-@interface MWStreamNode : SKNode
+@interface MWStreamNode : SKNode {
+@private
+    MWObjects *letterNodes;
+    SKLabelNode *letterNode;
+}
 
 @property (nonatomic, strong) NSString *letter;
 @property (nonatomic, copy) MWStreamLetterTouched letterTouched;
 @property (nonatomic, copy) MWStreamMaxDistanceReached maxDistanceReached;
 
-- (id)initWithLetter:(NSString *)letter;
+- (id)initWithLetter:(NSString *)letter inFrame:(CGRect)frame;
 
-// startup velocity for distance D, normal velocity after that
-// max distance M - if reached then call block
+- (void)startFallWithVelocity:(CGFloat)startupVelocity forDistance:(CGFloat)startupDistance normalVelocity:(CGFloat)normalVelocity forDistance:(CGFloat)normalVelocityDistance;
 
 @end
--- WordFall/SKNodes/MWStreamNode.m
+++ WordFall/SKNodes/MWStreamNode.m
@@ -7,8 +7,33 @@
 //
 
 #import "MWStreamNode.h"
+#import "MWObjects.h"
 
 @implementation MWStreamNode
 
+- (id)initWithLetter:(NSString *)letter inFrame:(CGRect)frame
+{
+    if ((self = [super init])) {
+        // create nodes
+    }
+    
+    return self;
+}
+
+- (void)startFallWithVelocity:(CGFloat)startupVelocity forDistance:(CGFloat)startupDistance normalVelocity:(CGFloat)normalVelocity forDistance:(CGFloat)normalVelocityDistance
+{
+    // startup velocity for distance D, normal velocity after that
+    // max distance M - if reached then call block
+}
+
+- (void)pause
+{
+    // pause on app inactive (ad tapped etc)
+}
+
+- (void)resume
+{
+    // resume on active
+}
 
 @end
