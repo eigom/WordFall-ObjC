485cce0ce7eec3667886f2a9812aefe774083812
eigo <eigo@eigos-mbp.lan>
2015-03-27 16:25:16 +0200

    .

--- WordFall/Core/MWPurchaseManager.h
+++ WordFall/Core/MWPurchaseManager.h
@@ -7,11 +7,24 @@
 //
 
 #import "MWManager.h"
+#import <StoreKit/StoreKit.h>
 
-@interface MWPurchaseManager : MWManager
+typedef void (^MWPurchaseManagerRequestProductsCompletion)(BOOL success, SKProduct *product);
+typedef void (^MWPurchaseManagerProductPurchasedCompletion)(SKProduct *product);
 
+@interface MWPurchaseManager : MWManager <SKProductsRequestDelegate, SKPaymentTransactionObserver> {
+@private
+    SKProductsRequest * _productsRequest;
+    MWPurchaseManagerRequestProductsCompletion _requestProductsCompletionHandler;
+}
+
+@property (nonatomic, readonly) SKProduct *product;
 @property (nonatomic, readonly) BOOL isPurchased;
+@property (nonatomic, copy) MWPurchaseManagerProductPurchasedCompletion productPurchasedCompletion;
 
 + (MWPurchaseManager *)sharedManager;
+
+- (void)requestProductsWithCompletionHandler:(MWPurchaseManagerRequestProductsCompletion)completionHandler;
+- (void)buy;
 
 @end
--- WordFall/Core/MWPurchaseManager.m
+++ WordFall/Core/MWPurchaseManager.m
@@ -8,6 +8,8 @@
 
 #import "MWPurchaseManager.h"
 
+static NSString * kProductIdentifier = @"";
+
 @implementation MWPurchaseManager
 
 + (MWPurchaseManager *)sharedManager
@@ -20,6 +22,127 @@
     });
     
     return manager;
+}
+
+- (id)init
+{
+    if ((self = [super init])) {
+        [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
+    }
+    
+    return self;
+}
+
+#pragma Load Product
+
+- (void)requestProductsWithCompletionHandler:(MWPurchaseManagerRequestProductsCompletion)completionHandler
+{
+    _requestProductsCompletionHandler = [completionHandler copy];
+    
+    _productsRequest = [[SKProductsRequest alloc] initWithProductIdentifiers:[NSSet setWithObject:kProductIdentifier]];
+    _productsRequest.delegate = self;
+    [_productsRequest start];
+}
+
+- (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response
+{
+    NSLog(@"Loaded list of products...");
+    _productsRequest = nil;
+    
+    if ([response.products count] > 0) {
+        NSLog(@"Loaded product");
+        _product = [response.products firstObject];
+    }
+    
+    if (_requestProductsCompletionHandler) {
+        _requestProductsCompletionHandler(YES, _product);
+        _requestProductsCompletionHandler = nil;
+    }
+}
+
+- (void)request:(SKRequest *)request didFailWithError:(NSError *)error
+{
+    NSLog(@"Failed to load list of products.");
+    _productsRequest = nil;
+    
+    if (_requestProductsCompletionHandler) {
+        _requestProductsCompletionHandler(NO, nil);
+        _requestProductsCompletionHandler = nil;
+    }
+}
+
+#pragma Purchase
+
+- (void)buy
+{
+    NSLog(@"Buying %@...", _product.productIdentifier);
+    
+    SKPayment * payment = [SKPayment paymentWithProduct:_product];
+    [[SKPaymentQueue defaultQueue] addPayment:payment];
+}
+
+- (void)provideContentForProductIdentifier:(NSString *)productIdentifier
+{
+    //TODO save receipt
+    [_purchasedProductIdentifiers addObject:productIdentifier];
+    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:productIdentifier];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+    if (_productPurchasedCompletion) {
+        _productPurchasedCompletion(_product);
+    }
+}
+
+- (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray *)transactions
+{
+    for (SKPaymentTransaction * transaction in transactions) {
+        switch (transaction.transactionState)
+        {
+            case SKPaymentTransactionStatePurchased:
+                [self completeTransaction:transaction];
+                break;
+            case SKPaymentTransactionStateFailed:
+                [self failedTransaction:transaction];
+                break;
+            case SKPaymentTransactionStateRestored:
+                [self restoreTransaction:transaction];
+            default:
+                break;
+        }
+    };
+}
+
+- (void)completeTransaction:(SKPaymentTransaction *)transaction
+{
+    NSLog(@"completeTransaction...");
+    
+    [self provideContentForProductIdentifier:transaction.payment.productIdentifier];
+    [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+}
+
+- (void)restoreTransaction:(SKPaymentTransaction *)transaction
+{
+    NSLog(@"restoreTransaction...");
+    
+    [self provideContentForProductIdentifier:transaction.originalTransaction.payment.productIdentifier];
+    [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+}
+
+- (void)failedTransaction:(SKPaymentTransaction *)transaction
+{
+    NSLog(@"failedTransaction...");
+    
+    if (transaction.error.code != SKErrorPaymentCancelled)
+    {
+        NSLog(@"Transaction error: %@", transaction.error.localizedDescription);
+    }
+    
+    [[SKPaymentQueue defaultQueue] finishTransaction: transaction];
+}
+
+- (BOOL)isPurchased
+{
+    // check receipt not nil
 }
 
 @end
